3a8cd98e09c9ae076649fd5508a7b6f0466cf109
Samhith657 <63170798+Samhith657@users.noreply.github.com>
2020-04-17 18:16:45 -0400

    Update Challenge3.py

--- Samhith/Challenge3.py
+++ Samhith/Challenge3.py
@@ -1,1 +1,13 @@
-
+import random
+ui = "y"
+while ui == "y":
+  randomnumber = (random.randrange(1,101))
+  playersguess = int ( input("Guess a number between 1-100"))
+  if playersguess == randomnumber:
+    print("You guessed correct!")
+  elif playersguess > randomnumber:
+   print("You guessed high!")
+  elif playersguess < randomnumber:
+    print("You guessed low!")   
+  
+  ui = (input("Do you want to play again? If yes, type y. If n, type no."))
