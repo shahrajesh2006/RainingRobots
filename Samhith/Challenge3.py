f2c8d6bab58e4e7ecdd0ecda65a89de7a7235a62
shahvr19 <63170773+shahvr19@users.noreply.github.com>
2020-04-18 14:24:19 -0400

    Update Challenge3.py
    Fixed the program to generate random number only once. Add more comments

--- Samhith/Challenge3.py
+++ Samhith/Challenge3.py
@@ -1,8 +1,9 @@
-import random
+import random #module to random generator
 ui = "y"
+randomnumber = random.randrange(1,101)# this will generate random number from 1-100
 while ui == "y":
-  randomnumber = (random.randrange(1,101))
-  playersguess = int ( input("Guess a number between 1-100"))
+  #randomnumber = (random.randrange(1,101))
+  playersguess = int ( input("Guess a number between 1-100"))#convert input to int type
   if playersguess == randomnumber:
     print("You guessed correct!")
   elif playersguess > randomnumber:
