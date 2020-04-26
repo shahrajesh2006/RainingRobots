451ad631652bc177d0f8be2b2692a7840580437d
shahrajesh2006 <shahrajesh2006@gmail.com>
2020-04-26 12:26:17 -0400

    Create Challenge1.py

---
+++ Vilpa/Challenge1.py
@@ -0,0 +1,9 @@
+vilpasnumbers=[1,7,3,6,9,20,5,6,8]
+
+for mynumber in vilpasnumbers:
+   if mynumber > 5:
+    print(mynumber)
+   else:
+     print("number less than five")
+
+print("end of program")
