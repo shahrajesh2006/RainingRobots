830640fbe0ddc6871d960c1f70c2e8a730f8a55a
shahrajesh2006 <shahrajesh2006@gmail.com>
2020-04-26 12:44:15 -0400

    Create BonusChallenge1.py

---
+++ Vilpa/BonusChallenge1.py
@@ -0,0 +1,8 @@
+userinput= input("Enter a number between 1 and 20: ")
+vilpasnumbers=[1,7,3,6,9,20,5,6,8]
+
+for mynumber in vilpasnumbers:
+   if mynumber > int(userinput): #convert string to int
+    print(mynumber)
+  
+print("end of program")
