e568183396b67302ed4e3d0f6be826ef71add002
shahrajesh2006 <shahrajesh2006@gmail.com>
2020-04-26 12:34:53 -0400

    Create challenge2.py

---
+++ Vilpa/challenge2.py
@@ -0,0 +1,7 @@
+cities=["richmond","new york", "chicago", "altanta","san francisco","austin","alexandria"]
+
+for mycity in cities:
+   if mycity.startswith("a"):
+    print(mycity)
+  
+print("end of program")
