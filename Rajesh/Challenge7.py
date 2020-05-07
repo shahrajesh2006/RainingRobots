552156dcb93e1a2fe8fc3aef031af4d081e75338
shahrajesh2006 <shahrajesh2006@gmail.com>
2020-05-07 19:01:43 -0400

    Update Challenge7.py

--- Rajesh/Challenge7.py
+++ Rajesh/Challenge7.py
@@ -3,57 +3,74 @@ import os
 import time
 from player import Player
 #Line 1-2 are the random number and operating system module 
+numberofpeopleplaying = input("How many people are playing? The max is 10 players.")
+time.sleep(1)
+os.system("clear")
 
-numberofplayers = input("How many players want to play- max 10")
 
 #Here is the object with three arguments
 players = []
 
 peopleplayingvalue = 0
-while peopleplayingvalue < int(numberofplayers):
-  print("Player" + str(peopleplayingvalue + 1) + " what is your name or a nickname.")
-  playersname=input("Enter here:") 
-  NewPerson = Player(playersname, random.randrange(1,101),0)
-  players.append(NewPerson)
-  peopleplayingvalue = peopleplayingvalue + 1
+
+while peopleplayingvalue < int(numberofpeopleplaying):
+ playersname = input("Player" + str(peopleplayingvalue + 1) + " what is your name or nickname")
+ NewPerson = Player(playersname, random.randrange(1,101), 0)
+ players.append(NewPerson)
+ print ("New player created..."+players[peopleplayingvalue].name)
+ peopleplayingvalue=peopleplayingvalue+1
+
+
+# players.append(NewPerson)
+# peopleplayingvalue = peopleplayingvalue + 1
+time.sleep(1)
+os.system("clear")
    
 
 peopleplayingvalue = 0
-while peopleplayingvalue < int(numberofplayers):
+while peopleplayingvalue < int(numberofpeopleplaying):
   players[peopleplayingvalue].PlayGame()
+  print ("after game")
   peopleplayingvalue = peopleplayingvalue + 1
-  time.sleep(2)
-  os.system ("clear")
-
-
-
-
-# Now lets identify who won. For this we need find player object which has least number of attempts
-peopleplayingvalue = 0
-winnerNumberofAttempts=players[0].attempts#lets assume winner number of attempts is first player
-winnerPlayer=0 # lets assume first player is the winner for now
-while peopleplayingvalue < int(numberofplayers):
-    if players[peopleplayingvalue].attempts<winnerNumberofAttempts:
-      winnerNumberofAttempts=players[peopleplayingvalue].attempts
-      winnerPlayer=peopleplayingvalue
 
 
 #This ask player 2 if they are ready
-Ready = input("Are you ready to find who won? Type y to continue")
-if Ready.lower().strip() =="y": 
- print ("Ok, lets continue!")
+print("Are")
+time.sleep(1)
+print("you")
+time.sleep(1)
+print("ready")
+time.sleep(1)
+print("to see")
+time.sleep(1)
+print("who")
+time.sleep(1)
+print("won?")
+ready = input("Print y to continue")
 
+if ready.lower().strip() =="y": 
+  print("Then lets... see... who... wonnnnnnnnn!")
 time.sleep(1)
 os.system ("clear") #Clears screen
 
-print("Winner is:"+players[winnerPlayer].name)
 
+WinningPlayer = players[0]# For now assume first player won
+print("For now the winning player is "+WinningPlayer.name)
+print(WinningPlayer)
 
-
-#Here I am writing the final stats
-print("Here are the final results")
 
 peopleplayingvalue = 0
-while peopleplayingvalue < int(numberofplayers):
-  players[peopleplayingvalue].PlayerResults()
+while peopleplayingvalue < int (numberofpeopleplaying):
+  time.sleep(1)
+  print("looping for the player"+players[peopleplayingvalue].name)
+  if players[peopleplayingvalue].attempts < WinningPlayer.attempts:
+   WinningPlayer = players[peopleplayingvalue]#found a better player so switch
+   print("Found the new winning player is "+WinningPlayer.name)
+  else:
+    print("Winning player is still the same "+WinningPlayer.name)
+  peopleplayingvalue = peopleplayingvalue + 1
+
+#With the above logic if there is tie then the first player will be the winner
   
+
+print("The winner is" + WinningPlayer.name)
