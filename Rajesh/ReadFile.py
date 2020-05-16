4946c9b3862d621b843a218231558ab72d6d7eb3
shahrajesh2006 <shahrajesh2006@gmail.com>
2020-05-16 18:52:22 +0000

    learning gitpod

--- Rajesh/ReadFile.py
+++ Rajesh/ReadFile.py
@@ -1,15 +1,15 @@
 # Python code to 
 # demonstrate readlines() 
 
-L = ["Geeks\n", "for\n", "Geeks\n"] 
+#L = ["Geeks\n", "for\n", "Geeks\n"] 
 
 # writing to file 
-file1 = open('myfile.txt', 'w') 
-file1.writelines(L) 
-file1.close() 
+#file1 = open('myfile1.txt', 'w') 
+#file1.writelines(L) 
+#file1.close() 
 
 # Using readlines() 
-file1 = open('myfile.txt', 'r') 
+file1 = open('Rajesh/myfile.txt', 'r') 
 Lines = file1.readlines() 
 
 count = 0
