066ff688c1ed3a8aa043f5a10a1c314c1c0f517d
shahrajesh2006 <shahrajesh2006@gmail.com>
2020-05-16 13:11:04 +0000

    added file

--- Rajesh/ReadFile.py
+++ Rajesh/ReadFile.py
@@ -1,12 +1,12 @@
 # Python code to 
 # demonstrate readlines() 
 
-#L = ["Geeks\n", "for\n", "Geeks\n"] 
+L = ["Geeks\n", "for\n", "Geeks\n"] 
 
 # writing to file 
-#file1 = open('myfile.txt', 'w') 
-#file1.writelines(L) 
-#file1.close() 
+file1 = open('myfile.txt', 'w') 
+file1.writelines(L) 
+file1.close() 
 
 # Using readlines() 
 file1 = open('myfile.txt', 'r') 
