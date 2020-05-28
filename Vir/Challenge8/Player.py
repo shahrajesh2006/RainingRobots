7610d2b2f7ceb22fe512309952a08c228b52f4ad
shahvr19 <shahvr19@gmail.com>
2020-05-28 19:30:38 +0000

    updated Vir's challenge 8

---
+++ Vir/Challenge8/Player.py
@@ -0,0 +1,40 @@
+import os
+import time
+
+class Player:
+
+  def __init__(self, playerName, randomNumber, numberOfAttempts):  
+    self.name=playerName
+    self.randomnumber= randomNumber
+    self.tries= numberOfAttempts
+
+  def incrementNumberOfAttempts(self):
+    self.tries=self.tries+1
+
+  def isGuessNumberValid(self,guessNumber):
+    if self.randomnumber==guessNumber:
+      print("You guessed right!")
+      return "valid"
+    elif self.randomnumber>guessNumber:
+      print("You guessed low!")
+    else:
+      print("You guessed high!")
+      
+  def printPlayerResults(self):
+    print("Player Name: " + self.name)
+    print("Your random number was "  + str(self.randomnumber))
+    print("Your number of attempts was " + str(self.tries))
+
+  def playGame(self):
+    input(self.name+" are u ready? Press any key to continue:")
+    os.system("clear")
+    print (self.name+"'s Turn")
+    while True:
+      x = int (input("Guess a number between 1 and 100:"))
+      self.incrementNumberOfAttempts()#increment number of attempts
+      result=self.isGuessNumberValid(x)
+      if(result=="valid"):
+        time.sleep(2)
+        os.system("clear")    
+        break
+        
