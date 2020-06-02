a0cb67a1ebd16d94296c592748319ce759275c31
shahvr19 <shahvr19@gmail.com>
2020-06-02 21:35:06 +0000

    commit message

--- Vir/Challenge8Bonus/Challenge8Bonus.py
+++ Vir/Challenge8Bonus/Challenge8Bonus.py
@@ -3,11 +3,16 @@ import os
 import time 
 from Player import Player
 
-Bestscore = open('Vir/Challenge8/bestscores.txt', 'r') 
+Bestscore = open('Vir/Challenge8Bonus/bestscores.txt', 'r') 
 Lines = Bestscore.readlines() 
 
 for line in Lines: 
 	print(line.strip())
+
+#reads the list of players
+OGPlayers = open('Vir/Challenge8Bonus/players.txt', 'r') 
+Lines2 = OGPlayers.readlines() 
+
 
 time.sleep(3)
 os.system("clear")
@@ -25,7 +30,10 @@ while playersplayingvalue < int(numberofplayers):
   playersname = input("Player " + str(playersplayingvalue + 1) + " what is your username: ") 
   NewPlayer = Player(playersname, random.randrange(1,101), 0)
   players.append(NewPlayer)#Adding new player to the list
-  print ("Player Created | "+players[playersplayingvalue].name)
+  NewPlayer.checkExisitingPlayer(Lines2) # check if it is an existing player
+  players.append(NewPlayer)
+
+  print ("Player Created | "+ players[playersplayingvalue].name)
   playersplayingvalue = playersplayingvalue+1 
 
 time.sleep(1) 
@@ -62,7 +70,12 @@ print("Thank You For Playing")
 
 time.sleep(1)
 
-Bestscore = open('Vir/Challenge8/bestscores.txt', 'w') 
+Bestscore = open('Vir/Challenge8Bonus/bestscores.txt', 'w') 
 Bestscore.writelines("Winner of the last game: " + players[0].name+"\n")
 Bestscore.writelines("Best Score for the last game: " + str(players[0].tries)+"\n")
 Bestscore.close()
+
+OGPlayers = open('Vir/Challenge8Bonus/players.txt', 'w') 
+for play in range(int(numberofplayers)):
+    OGPlayers.writelines(players[play].name+ "=" + str(players[play].tries) + "\n")
+OGPlayers.close()
