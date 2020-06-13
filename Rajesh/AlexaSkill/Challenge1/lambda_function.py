eac3f7b3be3f568cf07fd56c85fa93441e6a996f
shahrajesh2006 <shahrajesh2006@gmail.com>
2020-06-13 20:08:10 +0000

    clean up

--- Rajesh/AlexaSkill/Challenge1/lambda_function.py
+++ Rajesh/AlexaSkill/Challenge1/lambda_function.py
@@ -3,6 +3,8 @@
 # This is a High Low Guess game Alexa Skill.
 # The skill serves as a simple sample on how to use the
 # persistence attributes and persistence adapter features in the SDK.
+
+
 import random
 import logging
 import os
@@ -15,7 +17,7 @@ from ask_sdk_core.handler_input import HandlerInput
 from ask_sdk_model import Response
 from ask_sdk_s3.adapter import S3Adapter
 
-SKILL_NAME = 'High Low Game'
+SKILL_NAME = 'RR Game'
 bucket_name = os.environ.get('S3_PERSISTENCE_BUCKET')
 s3_client = boto3.client('s3', config=boto3.session.Config(signature_version='s3v4',s3={'addressing_style': 'path'}))
 s3_adapter = S3Adapter(bucket_name=bucket_name, path_prefix="Media", s3_client=s3_client)
@@ -25,6 +27,9 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
+SKILL_NAME = 'RR Game'
+
+#This is to handle the Launch Request or Invocation
 @sb.request_handler(can_handle_func=is_request_type("LaunchRequest"))
 def launch_request_handler(handler_input):
     """Handler for Skill Launch.
@@ -32,23 +37,62 @@ def launch_request_handler(handler_input):
     Get the persistence attributes, to figure out the game state.
     """
     # type: (HandlerInput) -> Response
-    attr = handler_input.attributes_manager.persistent_attributes
-    if not attr:
-        attr['ended_session_count'] = 0
-        attr['games_played'] = 0
-        attr['game_state'] = 'ENDED'
-
-    handler_input.attributes_manager.session_attributes = attr
+    
+    session_attr = handler_input.attributes_manager.session_attributes
+    session_attr['guess_number'] = random.randint(0, 100)#Generate the random number and store in the session
+    session_attr['no_of_guesses'] = 0
 
     speech_text = (
-        "Welcome to the High Low guessing game. You have played {} times. "
-        "Would you like to play?".format(attr["games_played"]))
-    reprompt = "Say yes to start the game or no to quit."
+        "Welcome to the High Low guessing game.Say a number between 1 and 100")
+
+    handler_input.response_builder.speak(speech_text).ask(speech_text)
+    return handler_input.response_builder.response
+
+#This is NumberGuessIntent
+@sb.request_handler(can_handle_func=lambda input:is_intent_name("NumberGuessIntent")(input))
+def number_guess_handler(handler_input):
+    """Handler for processing guess with target."""
+    # type: (HandlerInput) -> Response
+    session_attr = handler_input.attributes_manager.session_attributes
+    target_num = session_attr["guess_number"]
+    guess_num = int(handler_input.request_envelope.request.intent.slots[
+        "number"].value)
+
+    session_attr["no_of_guesses"] += 1#Increment number of guess by 1
+
+    if guess_num > target_num:
+        speech_text = (
+            "{} is too high. Try saying a smaller number.".format(guess_num))
+        reprompt = "Try saying a smaller number."
+    elif guess_num < target_num:
+        speech_text = (
+            "{} is too low. Try saying a larger number.".format(guess_num))
+        reprompt = "Try saying a larger number."
+    elif guess_num == target_num:
+        speech_text = (
+            "Congratulations. {} is the correct guess. "
+            "You guessed the number in {} guesses. "
+            "Would you like to play a new game?".format(
+                guess_num, session_attr["no_of_guesses"]))
+        reprompt = "Say yes to start a new game or no to end the game"
+    else:
+        speech_text = "Sorry, I didn't get that. Try saying a number."
+        reprompt = "Try saying a number."
 
     handler_input.response_builder.speak(speech_text).ask(reprompt)
     return handler_input.response_builder.response
 
+# Igore everything Below here for now----------------------------------------------------------------
 
+
+
+
+
+
+
+
+
+#This help intent
 @sb.request_handler(can_handle_func=is_intent_name("AMAZON.HelpIntent"))
 def help_intent_handler(handler_input):
     """Handler for Help Intent."""
@@ -62,7 +106,7 @@ def help_intent_handler(handler_input):
     handler_input.response_builder.speak(speech_text).ask(reprompt)
     return handler_input.response_builder.response
 
-
+#This intent will be for Cancel or Stop
 @sb.request_handler(
     can_handle_func=lambda input:
         is_intent_name("AMAZON.CancelIntent")(input) or
@@ -81,57 +125,33 @@ def cancel_and_stop_intent_handler(handler_input):
 def session_ended_request_handler(handler_input):
     """Handler for Session End."""
     # type: (HandlerInput) -> Response
+    
+    
     return handler_input.response_builder.response
 
 
-def currently_playing(handler_input):
-    """Function that acts as can handle for game state."""
-    # type: (HandlerInput) -> bool
-    is_currently_playing = False
-    session_attr = handler_input.attributes_manager.session_attributes
-
-    if ("game_state" in session_attr
-            and session_attr['game_state'] == "STARTED"):
-        is_currently_playing = True
-
-    return is_currently_playing
 
 
-@sb.request_handler(can_handle_func=lambda input:
-                    not currently_playing(input) and
-                    is_intent_name("AMAZON.YesIntent")(input))
+#This is yes intent
+@sb.request_handler(can_handle_func=lambda input:is_intent_name("AMAZON.YesIntent")(input))
 def yes_handler(handler_input):
     """Handler for Yes Intent, only if the player said yes for
     a new game.
     """
     # type: (HandlerInput) -> Response
-    session_attr = handler_input.attributes_manager.session_attributes
-    session_attr['game_state'] = "STARTED"
-    session_attr['guess_number'] = random.randint(0, 100)
-    session_attr['no_of_guesses'] = 0
-
+    
     speech_text = "Great! Try saying a number to start the game."
     reprompt = "Try saying a number."
 
     handler_input.response_builder.speak(speech_text).ask(reprompt)
     return handler_input.response_builder.response
 
-
-@sb.request_handler(can_handle_func=lambda input:
-                    not currently_playing(input) and
-                    is_intent_name("AMAZON.NoIntent")(input))
+#This is no intent
+@sb.request_handler(can_handle_func=lambda input:is_intent_name("AMAZON.NoIntent")(input))
 def no_handler(handler_input):
     """Handler for No Intent, only if the player said no for
     a new game.
     """
-    # type: (HandlerInput) -> Response
-    session_attr = handler_input.attributes_manager.session_attributes
-    session_attr['game_state'] = "ENDED"
-    session_attr['ended_session_count'] += 1
-
-    handler_input.attributes_manager.persistent_attributes = session_attr
-    handler_input.attributes_manager.save_persistent_attributes()
-
     speech_text = "Ok. See you next time!!"
 
     handler_input.response_builder.speak(speech_text)
@@ -139,50 +159,7 @@ def no_handler(handler_input):
 
 
 @sb.request_handler(can_handle_func=lambda input:
-                    currently_playing(input) and
-                    is_intent_name("NumberGuessIntent")(input))
-def number_guess_handler(handler_input):
-    """Handler for processing guess with target."""
-    # type: (HandlerInput) -> Response
-    session_attr = handler_input.attributes_manager.session_attributes
-    target_num = session_attr["guess_number"]
-    guess_num = int(handler_input.request_envelope.request.intent.slots[
-        "number"].value)
-
-    session_attr["no_of_guesses"] += 1
-
-    if guess_num > target_num:
-        speech_text = (
-            "{} is too high. Try saying a smaller number.".format(guess_num))
-        reprompt = "Try saying a smaller number."
-    elif guess_num < target_num:
-        speech_text = (
-            "{} is too low. Try saying a larger number.".format(guess_num))
-        reprompt = "Try saying a larger number."
-    elif guess_num == target_num:
-        speech_text = (
-            "Congratulations. {} is the correct guess. "
-            "You guessed the number in {} guesses. "
-            "Would you like to play a new game?".format(
-                guess_num, session_attr["no_of_guesses"]))
-        reprompt = "Say yes to start a new game or no to end the game"
-        session_attr["games_played"] += 1
-        session_attr["game_state"] = "ENDED"
-
-        handler_input.attributes_manager.persistent_attributes = session_attr
-        handler_input.attributes_manager.save_persistent_attributes()
-    else:
-        speech_text = "Sorry, I didn't get that. Try saying a number."
-        reprompt = "Try saying a number."
-
-    handler_input.response_builder.speak(speech_text).ask(reprompt)
-    return handler_input.response_builder.response
-
-
-@sb.request_handler(can_handle_func=lambda input:
-                    is_intent_name("AMAZON.FallbackIntent")(input) or
-                    is_intent_name("AMAZON.YesIntent")(input) or
-                    is_intent_name("AMAZON.NoIntent")(input))
+                    is_intent_name("AMAZON.FallbackIntent")(input))
 def fallback_handler(handler_input):
     """AMAZON.FallbackIntent is only available in en-US locale.
     This handler will not be triggered except in that locale,
@@ -191,24 +168,11 @@ def fallback_handler(handler_input):
     # type: (HandlerInput) -> Response
     session_attr = handler_input.attributes_manager.session_attributes
 
-    if ("game_state" in session_attr and
-            session_attr["game_state"]=="STARTED"):
-        speech_text = (
-            "The {} skill can't help you with that.  "
-            "Try guessing a number between 0 and 100. ".format(SKILL_NAME))
-        reprompt = "Please guess a number between 0 and 100."
-    else:
-        speech_text = (
-            "The {} skill can't help you with that.  "
-            "It will come up with a number between 0 and 100 and "
-            "you try to guess it by saying a number in that range. "
-            "Would you like to play?".format(SKILL_NAME))
-        reprompt = "Say yes to start the game or no to quit."
-
-    handler_input.response_builder.speak(speech_text).ask(reprompt)
+    speech_text ="I did not understand what to do"
+    handler_input.response_builder.speak(speech_text).ask(speech_text)
     return handler_input.response_builder.response
 
-
+#This when the Alexa skill is not able to find a skill based utterance
 @sb.request_handler(can_handle_func=lambda input: True)
 def unhandled_intent_handler(handler_input):
     """Handler for all other unhandled requests."""
@@ -217,17 +181,20 @@ def unhandled_intent_handler(handler_input):
     handler_input.response_builder.speak(speech).ask(speech)
     return handler_input.response_builder.response
 
-
+#This is when code will throw any error
 @sb.exception_handler(can_handle_func=lambda i, e: True)
 def all_exception_handler(handler_input, exception):
     """Catch all exception handler, log exception and
     respond with custom message.
     """
     # type: (HandlerInput, Exception) -> Response
-    logger.error(exception, exc_info=True)
     speech = "Sorry, I can't understand that. Please say again!!"
     handler_input.response_builder.speak(speech).ask(speech)
     return handler_input.response_builder.response
+
+#The SkillBuilder object acts as the entry point for your skill, routing all request and response
+# payloads to the handlers above. Make sure any new handlers or interceptors you've
+# defined are included below. The order matters - they're processed top to bottom.
 
 
 @sb.global_response_interceptor()
@@ -235,6 +202,5 @@ def log_response(handler_input, response):
     """Response logger."""
     # type: (HandlerInput, Response) -> None
     logger.info("Response: {}".format(response))
-
 
 lambda_handler = sb.lambda_handler()
