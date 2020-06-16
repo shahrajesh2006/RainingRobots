3e144102639d0353458bcd85629d17f4db5c60d3
shahrajesh2006 <shahrajesh2006@gmail.com>
2020-06-16 22:06:23 +0000

    alexa

--- Rajesh/AlexaSkill/Challenge1/lambda_function.py
+++ Rajesh/AlexaSkill/Challenge1/lambda_function.py
@@ -39,7 +39,7 @@ def launch_request_handler(handler_input):
     # type: (HandlerInput) -> Response
     
     session_attr = handler_input.attributes_manager.session_attributes
-    session_attr['guess_number'] = random.randint(0, 100)#Generate the random number and store in the session
+    session_attr['guess_number'] = 50#random.randint(0, 100)#Generate the random number and store in the session
     session_attr['no_of_guesses'] = 0
 
     speech_text = (
@@ -87,99 +87,12 @@ def number_guess_handler(handler_input):
 # Igore everything Below here for now----------------------------------------------------------------
 
 
-
-
-
-
-
-
-
-#This help intent
-@sb.request_handler(can_handle_func=is_intent_name("AMAZON.HelpIntent"))
-def help_intent_handler(handler_input):
-    """Handler for Help Intent."""
-    # type: (HandlerInput) -> Response
-    speech_text = (
-        "I am thinking of a number between zero and one hundred, try to "
-        "guess it and I will tell you if you got it or it is higher or "
-        "lower")
-    reprompt = "Try saying a number."
-
-    handler_input.response_builder.speak(speech_text).ask(reprompt)
-    return handler_input.response_builder.response
-
-#This intent will be for Cancel or Stop
-@sb.request_handler(
-    can_handle_func=lambda input:
-        is_intent_name("AMAZON.CancelIntent")(input) or
-        is_intent_name("AMAZON.StopIntent")(input))
-def cancel_and_stop_intent_handler(handler_input):
-    """Single handler for Cancel and Stop Intent."""
-    # type: (HandlerInput) -> Response
-    speech_text = "Thanks for playing!!"
-
-    handler_input.response_builder.speak(
-        speech_text).set_should_end_session(True)
-    return handler_input.response_builder.response
-
-
-@sb.request_handler(can_handle_func=is_request_type("SessionEndedRequest"))
-def session_ended_request_handler(handler_input):
-    """Handler for Session End."""
-    # type: (HandlerInput) -> Response
-    
-    
-    return handler_input.response_builder.response
-
-
-
-
-#This is yes intent
-@sb.request_handler(can_handle_func=lambda input:is_intent_name("AMAZON.YesIntent")(input))
-def yes_handler(handler_input):
-    """Handler for Yes Intent, only if the player said yes for
-    a new game.
-    """
-    # type: (HandlerInput) -> Response
-    
-    speech_text = "Great! Try saying a number to start the game."
-    reprompt = "Try saying a number."
-
-    handler_input.response_builder.speak(speech_text).ask(reprompt)
-    return handler_input.response_builder.response
-
-#This is no intent
-@sb.request_handler(can_handle_func=lambda input:is_intent_name("AMAZON.NoIntent")(input))
-def no_handler(handler_input):
-    """Handler for No Intent, only if the player said no for
-    a new game.
-    """
-    speech_text = "Ok. See you next time!!"
-
-    handler_input.response_builder.speak(speech_text)
-    return handler_input.response_builder.response
-
-
-@sb.request_handler(can_handle_func=lambda input:
-                    is_intent_name("AMAZON.FallbackIntent")(input))
-def fallback_handler(handler_input):
-    """AMAZON.FallbackIntent is only available in en-US locale.
-    This handler will not be triggered except in that locale,
-    so it is safe to deploy on any locale.
-    """
-    # type: (HandlerInput) -> Response
-    session_attr = handler_input.attributes_manager.session_attributes
-
-    speech_text ="I did not understand what to do"
-    handler_input.response_builder.speak(speech_text).ask(speech_text)
-    return handler_input.response_builder.response
-
 #This when the Alexa skill is not able to find a skill based utterance
 @sb.request_handler(can_handle_func=lambda input: True)
 def unhandled_intent_handler(handler_input):
     """Handler for all other unhandled requests."""
     # type: (HandlerInput) -> Response
-    speech = "Say yes to continue or no to end the game!!"
+    speech = "I am not sure what to do!"
     handler_input.response_builder.speak(speech).ask(speech)
     return handler_input.response_builder.response
 
